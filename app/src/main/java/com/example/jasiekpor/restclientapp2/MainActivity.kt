5a1d03d933052506338b8a2e82ec759630b58f98
Jasiek Porowski <jasiekpor@MacBook-Pro-Jasiek.local>
2016-01-15 10:38:47 +0100

    layout

--- app/src/main/java/com/example/jasiekpor/restclientapp2/MainActivity.kt
+++ app/src/main/java/com/example/jasiekpor/restclientapp2/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.example.jasiekpor.restclientapp2
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.Toolbar
-import android.view.View
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
